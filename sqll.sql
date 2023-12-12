7de09fd149d60b10f759bc95fe584fa1a0874fac
QhibodGit <hisaquadri@gmail.com>
2023-12-12 18:38:41 +0100

    hh

--- sqll.sql
+++ sqll.sql
@@ -2,4 +2,5 @@ CREATE TABLE james( Name VARCHAR(10),
                          age int);
 SELECT age from james;
 
-insert into james VALUES ('mama',23);
+insert into james VALUES ('mama',23),
+insert into james VALUES ('ma',03);
